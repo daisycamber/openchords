0b9d28ed239ddba9826853db73baf81dfcccecf7
Jasper Holton <jasper.camber.holton@gmail.com>
2019-03-06 02:16:32 +0000

    still trying

--- main/scroll.js
+++ main/scroll.js
@@ -12,7 +12,7 @@ function scroll(){
   var lastY = 0
   var lastX = 0
   stage.on("stagemousemove", function(evt) {
-    if(mouseDown && lastY > 0 && lastX > 0) {// && pianoKeys.y + evt.stageY - lastY <= KEYSIZE * 50 && pianoKeys.y + evt.stageY - lastY >=KEYSIZE * -40){
+    if(lastY > 0 && lastX > 0) {// && pianoKeys.y + evt.stageY - lastY <= KEYSIZE * 50 && pianoKeys.y + evt.stageY - lastY >=KEYSIZE * -40){
       pianoKeysContainer.y = pianoKeysContainer.y + evt.stageY - lastY
       chordsContainer.y = chordsContainer.y + evt.stageY - lastY
     }
